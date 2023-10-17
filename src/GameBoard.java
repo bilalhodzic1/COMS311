f54070d1b89b65520334e3fb87f6b9018d5b4268
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2023-10-17 17:34:18 -0500

    Initilazied gameboard to all -1 started file read loop

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,12 +1,29 @@
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GameBoard {
+    public int[][] boardArray = new int[6][6];
+    public Vehicle[] vehicles;
     public GameBoard(){
-        //TODO
+        //Initialize board array to all -1 signfyign empty spots
+        for(int i = 0; i < boardArray.length; i++){
+            for(int j = 0; j < boardArray[0].length; j++){
+                boardArray[i][j] = -1;
+            }
+        }
     }
     public void readInput(String FileName) throws IOException {
-        //TODO
+        try{
+            File file = new File(FileName);
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()){
+
+            }
+        }catch (Exception e){
+            throw new IOException();
+        }
     }
 
     public ArrayList<Pair> getPlan(){
