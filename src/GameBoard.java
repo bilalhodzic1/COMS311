9ad78f3bb7d062abeac1d80a41031f72a735f9c7
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2023-10-17 16:50:31 -0500

    Created valid method stubs for pair and gameboard classes

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,2 +1,20 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class GameBoard {
+    public GameBoard(){
+        //TODO
+    }
+    public void readInput(String FileName) throws IOException {
+        //TODO
+    }
+
+    public ArrayList<Pair> getPlan(){
+        //TODO
+        return null;
+    }
+    public int getNumOfPaths(){
+        //TODO
+        return -1;
+    }
 }
--- src/Pair.java
+++ src/Pair.java
@@ -1,2 +1,20 @@
 public class Pair {
+    int id;
+    char direction;
+    public Pair(int i, char d){
+        direction = d;
+        id = i;
+    }
+    public char getDirection(){
+        return direction;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int i){
+        id = i;
+    }
+    public void setDirection(char d){
+        direction = d;
+    }
 }
