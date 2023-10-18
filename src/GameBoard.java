301ca72cc89061bf86cfcddc9014368711369c9f
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2023-10-17 19:59:37 -0500

    Gets all cars with conveted coords correctly at this stage. Does not fill the cars

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -6,35 +6,56 @@ import java.util.Scanner;
 public class GameBoard {
     public int[][] boardArray = new int[6][6];
     public Vehicle[] vehicles;
-    public GameBoard(){
+
+    public GameBoard() {
         //Initialize board array to all -1 signfyign empty spots
-        for(int i = 0; i < boardArray.length; i++){
-            for(int j = 0; j < boardArray[0].length; j++){
+        for (int i = 0; i < boardArray.length; i++) {
+            for (int j = 0; j < boardArray[0].length; j++) {
                 boardArray[i][j] = -1;
             }
         }
     }
+
     public void readInput(String FileName) throws IOException {
-        try{
+        try {
             File file = new File(FileName);
             Scanner scanner = new Scanner(file);
-            while(scanner.hasNextLine()){
-                String line = scanner.nextLine();
+            String line = scanner.nextLine();
+            vehicles = new Vehicle[Integer.parseInt(line)];
+            int currVehicleIndex = 0;
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
                 String[] splitLine = line.split("\\s+");
-                for(String str : splitLine){
-                    System.out.println(str);
+                Vehicle newVehicle = new Vehicle();
+                int[] coords = new int[3];
+                int coordsIndex = 0;
+                for (String str : splitLine) {
+                    coords[coordsIndex] = Integer.parseInt(str) - 1;
+                    coordsIndex++;
                 }
+                newVehicle.vehicleId = currVehicleIndex;
+                newVehicle.xCoord = coords[0] % 6;
+                newVehicle.yCoord = coords[0] / 6;
+                newVehicle.leftRight = coords[0] + 1 == coords[1];
+                if(coords[2] != 0){
+                    newVehicle.length = 3;
+                }else{
+                    newVehicle.length = 2;
+                }
+                vehicles[currVehicleIndex] = newVehicle;
+                currVehicleIndex++;
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new IOException();
         }
     }
 
-    public ArrayList<Pair> getPlan(){
+    public ArrayList<Pair> getPlan() {
         //TODO
         return null;
     }
-    public int getNumOfPaths(){
+
+    public int getNumOfPaths() {
         //TODO
         return -1;
     }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,4 +1,12 @@
 public class Vehicle {
     public int vehicleId;
     public boolean leftRight;
+
+    //Always leftmost x coord which is lowest actual value
+    public int xCoord;
+
+    //Always highest on grid y value which is lowest actual value
+    public int yCoord;
+
+    public int length;
 }
